e678c2efefe842c026632d5f2d834124402e82b2
Maxime Monnier <maximemonnier39@gmail.com>
2023-05-08 16:03:46 +0400

    annimation background ok, text on canvas ok

--- script.js
+++ script.js
@@ -22,11 +22,19 @@ let index = 0,
 // fucntion animation
 const render = () => {
     index++;
+    // animation du background
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, - ((index* (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+    // Création d'un decuxieme background pour fluidifier l'annimation
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, - ((index* (speed / 2)) % canvas.width), 0, canvas.width, canvas.height);
 
-    //découpe et replacement de l'image e qui crée lanimation utilisation des paramètre plus haut
+    //découpe et replacement de l'image de loiseau qui crée lanimation utilisation des paramètre plus haut
     ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width /2) - size [0] / 2), flyHeight, ...size);
     flyHeight = (canvas.height / 2) - (size[1] / 2);
-
+    // methode pour écrie dans la canvas
+    ctx.fillText(`meilleur score : ${bestScore}`
+    , 55, 245);
+    ctx.fillText('Cliquer pour jouer', 48, 535);
+    ctx.font = "bold 30px courier";
     // fait jouer en boucle la focntion render
     window.requestAnimationFrame(render);
 }
